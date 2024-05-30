f358f7dbb090ba4a90cba9093afc52fe7368f1c4
mateus silva <mateussilvamn18@gmail.com>
2024-05-30 08:53:04 -0300

    yelp mvp

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+dishoom = { name: 'Dishoom', address: '7 Boundary St, London E2 7JE', phone_number:'40028922',category: 'chinese' }
+pizza_east = { name: 'Pizza East', address: '56A Shoreditch High St, London E1 6PQ', phone_number:'40028923', category: 'belgian' }
+mequi = { name: 'Mequi 1000', address: 'Av. Paulista, 1000', phone_number:'40028924', category: 'italian' }
+cao_veio = { name: 'Cão Véio', address: 'R. Mourato Coelho', phone_number:'40028925', category: 'japanese' }
+le_dog = { name: 'Le dog', address: 'R. Mourato Coelho', phone_number:'40028926',  category: 'french' }
+
+[dishoom, pizza_east, mequi, cao_veio, le_dog].each do |attributes|
+ Restaurant.create!(attributes)
+end
